eff33ab9901d78dbe193a8bfb868266ed9364277
Claire Hsu <clairehsu86@gmail.com>
2016-11-13 13:56:40 -0800

    add solutions for balancedParens.js and maxQueue.js

--- src/balancedParens.js
+++ src/balancedParens.js
@@ -1,8 +1,46 @@
 class Stack {
   // Your code here
+  constructor() {
+    this.items = [];
+    this.count = 0;
+  }
+  
+  getLength() {
+    return this.count;
+  }
+  
+  push(item) {
+    this.items.push(item);
+    this.count = this.count + 1;
+  }
+  
+  pop() {
+    if(this.count > 0) {
+      this.count = this.count - 1;
+    }
+    
+    return this.items.pop();
+  }
+  
+  peek() {
+    return this.items.slice(-1)[0];
+  }
 };
 
 const balancedParens = (str) => {
   // Your code here
-
+  let stack = new Stack();
+  let open = { '{': '}', '[': ']', '(': ')' };
+  let closed = { '}': true, ']': true, ')': true };
+  
+  for (var i = 0; i < str.length; i ++) {
+    let chr = str[i];
+    if (open[chr]) {
+      stack.push(chr);
+    } else if (closed[chr]) {
+    	let popped = open[stack.pop()];
+      if (popped !== chr) return false;
+    }
+  }
+  return stack.count === 0;
 };
--- src/maxQueue.js
+++ src/maxQueue.js
@@ -1,4 +1,44 @@
 class Queue {
   // Your code here
-
+  constructor() {
+    this.items = [];
+    this.count = 0;
+    this.max = [];
+  }
+  
+  getLength() {
+    return this.count;
+  }
+  
+  enqueue(item) {
+    this.items.unshift(item);
+    this.count = this.count + 1;
+    if(this.max.length === 0 || this.max[0] <= item){
+      this.max.unshift(item);
+    }
+  }
+  
+  dequeue() {
+    if(this.count > 0) {
+      this.count = this.count - 1;
+    }
+    var item = this.items.shift();
+    if(item === this.max[0]){
+      this.max.shift();
+    }
+    if(this.max.length === 0 ){
+      var tempMax; 
+      for(var i = 0; i < this.items.length; i++){
+        if(tempMax < this.items[i]){
+          tempMax = this.item[i];
+        }
+      }
+      this.max[0] = tempMax;
+    }
+    return item;
+  }
+  
+  peek() {
+    return this.items.slice(0,1)[0];
+  }
 };
